de60a669098a2189de8e97f95fa7c53dd4c75412
Harsh <harshsoni566@gmail.com>
2019-07-20 02:46:02 +0530

    First Commit

--- Index.py
+++ Index.py
@@ -1,3 +1,4 @@
+#TRIAL
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name='bit',
+    version='0.1',
+    packages=['venv.Lib.site-packages.PyQt5'],
+    url='www.alivecreate.com',
+    license='All Rights Reserved by AliveCreate',
+    author='AliveCreate',
+    author_email='info@alivecreate.com',
+    description='Billing Software'
+)
